63138cdb1a87c62a3108b1e2a9f36bc69c9d3b08
Aleksander Kapera <aleksanderkapera@gmail.com>
2024-04-29 11:04:42 -0400

    Add poppins font

--- lib/styles/text_styles.dart
+++ lib/styles/text_styles.dart
@@ -2,15 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:forma_app/styles/app_colors.dart';
 
 class TextStyles {
-  static const _galanoGrotesque = 'GalanoGrotesque';
+  static const _poppins = 'Poppins';
   static const _presentation = 'presentation';
 
   static const baseTextStyle =
-  TextStyle(fontFamily: _galanoGrotesque, package: _presentation);
+      TextStyle(fontFamily: _poppins, package: _presentation);
 
   static final lightTextStyle =
-  baseTextStyle.copyWith(color: AppColors.pureWhite);
+      baseTextStyle.copyWith(color: AppColors.pureWhite);
 
   static final darkTextStyle =
-  baseTextStyle.copyWith(color: AppColors.pureBlack);
+      baseTextStyle.copyWith(color: AppColors.pureBlack);
 }
