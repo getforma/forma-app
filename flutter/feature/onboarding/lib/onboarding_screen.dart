a927606c64f9c955003ffdebc0bdec43c9f207c5
Aleksander Kapera <aleksanderkapera@gmail.com>
2024-11-17 20:00:01 -0500

    Add onboarding screen body

--- flutter/feature/onboarding/lib/onboarding_screen.dart
+++ flutter/feature/onboarding/lib/onboarding_screen.dart
@@ -1,9 +1,12 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:core_feature/generated/l10n.dart';
 import 'package:core_feature/style/app_colors.dart';
+import 'package:core_feature/style/button_styles.dart';
+import 'package:core_feature/style/text_styles.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get_it/get_it.dart';
 import 'package:onboarding_feature/bloc/onboarding_cubit.dart';
 
@@ -19,9 +22,10 @@ class OnboardingScreen extends StatelessWidget {
         builder: (context, state) => Scaffold(
           resizeToAvoidBottomInset: false,
           body: Stack(
+            fit: StackFit.expand,
             children: [
-              _background(context),
-              _body(context, state),
+              Positioned.fill(child: _background(context)),
+              Positioned.fill(child: _body(context, state)),
             ],
           ),
         ),
@@ -29,44 +33,87 @@ class OnboardingScreen extends StatelessWidget {
     );
   }
 
-  Widget _background(BuildContext context) => Expanded(
-        child: Stack(
-          children: [
-            Positioned.fill(
-              child: Image.asset(
-                'asset/onboarding_background.png',
-                fit: BoxFit.cover,
-              ),
+  Widget _background(BuildContext context) => Stack(
+        children: [
+          Positioned.fill(
+            child: Image.asset(
+              'asset/background.png',
+              package: 'onboarding_feature',
+              fit: BoxFit.cover,
             ),
-            Positioned(
-              left: 0,
-              right: 0,
-              top: 174.h,
-              bottom: -66.h,
-              child: Container(
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                    colors: [
-                      AppColors.appBlack.withOpacity(0),
-                      AppColors.appBlack,
-                    ],
-                    begin: Alignment.topCenter,
-                    end: Alignment.bottomCenter,
-                  ),
+          ),
+          Positioned(
+            left: 0,
+            right: 0,
+            top: 174.h,
+            bottom: -66.h,
+            child: Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    AppColors.appBlack.withOpacity(0),
+                    AppColors.appBlack,
+                  ],
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
                 ),
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       );
 
   Widget _body(BuildContext context, OnboardingState state) => Container(
-        color: AppColors.background,
-        padding: EdgeInsets.symmetric(horizontal: 16.w),
+        padding: EdgeInsets.symmetric(horizontal: 32.w),
         child: SafeArea(
           left: false,
           right: false,
-          child: Container(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              41.verticalSpace,
+              SvgPicture.asset(
+                'asset/logo_black.svg',
+                package: 'onboarding_feature',
+                height: 52.h,
+                fit: BoxFit.fitHeight,
+              ),
+              Expanded(child: Container()),
+              Text(
+                S.of(context).onboarding_title,
+                style: _Typography.title.sp,
+              ),
+              16.verticalSpace,
+              Text(
+                S.of(context).onboarding_description,
+                style: _Typography.description.sp,
+              ),
+              16.verticalSpace,
+              TextButton(
+                onPressed: () {},
+                style: ButtonStyles.fullWidthWhite.sp,
+                child: Text(
+                  S.of(context).onboarding_get_started,
+                  style: TextStyles.darkBold16.sp,
+                ),
+              ),
+              21.verticalSpace,
+              Text(
+                S.of(context).onboarding_log_in,
+                style: TextStyles.lightSemiBold14
+                    .copyWith(color: const Color(0xFF4181FE))
+                    .sp,
+                textAlign: TextAlign.center,
+              ),
+              16.verticalSpace,
+            ],
+          ),
         ),
       );
+}
+
+class _Typography {
+  static final title = TextStyles.lightExtraBold48.copyWith(height: 1.2);
+  static final description =
+      TextStyles.lightRegular16.copyWith(height: 21 / 16, letterSpacing: 0);
 }
