dafda3a5b4acf7be744e220c6bbded231d7a1dd7
Aleksander Kapera <aleksanderkapera@gmail.com>
2024-12-13 20:17:24 -0500

    Add TTS service

---
+++ flutter/component/text_to_speech/data/lib/text_to_speech_service.dart
@@ -0,0 +1,20 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
+import 'package:injectable/injectable.dart';
+import 'package:retrofit/retrofit.dart';
+import 'package:text_to_speech_component_data/model/text_to_speech_request.dart';
+
+part 'text_to_speech_service.g.dart';
+
+@RestApi()
+@lazySingleton
+abstract class TextToSpeechService {
+  @factoryMethod
+  factory TextToSpeechService(Dio dio) => _TextToSpeechService(dio,
+      baseUrl: "https://getspeechfromtext-3pukyo7nvq-uc.a.run.app/");
+
+  @POST("")
+  @DioResponseType(ResponseType.bytes)
+  Future<HttpResponse<List<int>>> getSpeechFromText(
+      @Body() TextToSpeechRequest body);
+}
